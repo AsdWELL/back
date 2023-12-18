24f28d5b0f87b54b067094a1cdb54c1b2b2dd087
AsdWELL <dimagodov.23@mail.ru>
2023-12-18 19:19:14 +0300

    v4
    добавлена возможность сохранять аватарку

--- src/main/java/com/university/back/controller/MemberController.java
+++ src/main/java/com/university/back/controller/MemberController.java
@@ -1,18 +1,27 @@
 package com.university.back.controller;
 
+import com.university.back.model.Form;
 import com.university.back.model.Member;
 import com.university.back.service.MemberService;
+import jakarta.annotation.Nullable;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/student")
 @AllArgsConstructor
 @CrossOrigin
+@Slf4j
+@Transactional
 public class MemberController {
     private MemberService service;
 
@@ -32,7 +41,7 @@ public class MemberController {
     }
 
     @GetMapping(value = "/memberData", produces = "application/json")
-    public HashMap<String, String> getMemberData(@RequestParam("sessionId") String sessionId) {
+    public HashMap<String, String> getMemberData(@RequestParam("sessionId") String sessionId) throws IOException {
         var member = service.getMemberBySessionId(sessionId);
         if (member == null)
             return null;
@@ -43,5 +52,25 @@ public class MemberController {
     public String getDate() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
         return LocalDateTime.now().format(formatter);
+    }
+
+    @PostMapping(value = "/updateMember", consumes = "multipart/form-data", produces = "application/json")
+    public HashMap<String, String> updateMember(@RequestPart("file") @Nullable MultipartFile file, @RequestPart("login") String login, @RequestPart("member") Member member) throws IOException {
+        return service.updateMember(login, member, file);
+    }
+    @GetMapping(value = "/myGroup", produces = "application/json")
+    public List<Object[]> getStudentsByGroup(@RequestParam("group") String group) {
+        return service.getStudentsByGroup(group.toUpperCase());
+    }
+
+    @GetMapping(value = "/members", produces = "application/json")
+    public List<Member> getAllMembers(@RequestParam("sessionId") String sessionId) {
+        return service.getAllMembers(sessionId);
+    }
+
+    @DeleteMapping(value = "/deleteMember")
+    public void deleteMember(@RequestParam("login") String login) {
+        log.info(login);
+        service.deleteMember(login);
     }
 }
--- src/main/java/com/university/back/service/MemberService.java
+++ src/main/java/com/university/back/service/MemberService.java
@@ -2,21 +2,27 @@ package com.university.back.service;
 
 import com.university.back.model.Member;
 import com.university.back.repository.MemberRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.util.*;
 
 @Service
+@Transactional
 @AllArgsConstructor
+@Slf4j
 public class MemberService{
     private MemberRepository repository;
 
     public HashMap<String, String> saveMember(Member member) {
         HashMap<String, String> response = new HashMap<>();
-        member.refactor();
+        member.setDefaults();
         if (repository.findMemberByLogin(member.getLogin()) == null) {
             member.setSessionId(RequestContextHolder.currentRequestAttributes().getSessionId());
             response.put("sessionId", member.getSessionId());
@@ -46,21 +52,64 @@ public class MemberService{
     public void deleteMember(String login) {
         repository.deleteMemberByLogin(login);
     }
-    public Member getMemberByLogin(String login) {
-        return repository.findMemberByLogin(login);
+
+    public HashMap<String, String> updateMember(String login, Member newMember, MultipartFile file) throws IOException {
+        HashMap<String, String> response = new HashMap<>();
+        var member = repository.findMemberByLogin(login);
+        newMember.refactor();
+        if (!newMember.getRole().equals("student"))
+            newMember.setGroup("");
+        newMember.setVisitCounter(member.getVisitCounter());
+        newMember.setSessionId(member.getSessionId());
+        if (file != null)
+            newMember.setImg(file.getBytes());
+        else
+            newMember.setImg(member.getImg());
+        if (login.equals(newMember.getLogin())) {
+            repository.save(newMember);
+            response.put("updated", "true");
+        }
+        else {
+            if (repository.findMemberByLogin(newMember.getLogin()) != null)
+                response.put("error_login", "Данный логин уже занят");
+            else {
+                repository.updateMemberByLogin(login, newMember);
+                response.put("updated", "true");
+            }
+        }
+        return response;
     }
+
     public Member getMemberBySessionId(String sessionId) {
         return repository.findMemberBySessionId(sessionId);
     }
+    public List<Object[]> getStudentsByGroup(String group) {
+        return repository.findAllByGroup(group);
+    }
+    public List<Member> getAllMembers(String sessionId) {
+        var member = repository.findMemberBySessionId(sessionId);
+        if (member == null)
+            return null;
+        if (!member.getRole().equals("admin"))
+            return null;
+        return repository.findAllMembers();
+    }
 
-    public HashMap<String, String> getMemberData(Member member) {
+    public HashMap<String, String> getMemberData(Member member) throws IOException {
         HashMap<String, String> response = new HashMap<>();
         response.put("name", member.getName());
         response.put("surname", member.getSurname());
         response.put("login", member.getLogin());
         response.put("group", member.getGroup());
         response.put("role", member.getRole());
+        response.put("password", member.getPassword());
         response.put("counter", String.valueOf(member.getVisitCounter()));
+        if (member.getImg() != null) {
+            ByteArrayResource file = new ByteArrayResource(member.getImg());
+            response.put("img", Base64.getEncoder().encodeToString(member.getImg()));
+        }
+        else
+            response.put("img", null);
         return response;
     }
 }
